af004cf87f07069d8e457dd48dcb8245769529fa
abduladil sunnat <abduladilsunnat@gmail.com>
2022-12-19 13:11:59 -0600

    ui fixes

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -8,12 +8,26 @@ import { IPopularMovie } from 'src/app/services/movies/types';
 })
 export class BodyComponent implements OnInit {
   public popularMovies: IPopularMovie[] = [];
-
+  public testData: {
+    title: string;
+    rating: number;
+    reviews: number;
+    release: number;
+    description: string;
+  } = {
+    title: 'Emancipation',
+    rating: 8.2,
+    reviews: 333,
+    release: 2022,
+    description:
+      'Inspired by the gripping true story of a man who would do anything for his family—and for freedom. When Peter, an enslaved man, risks his life to escape and return to his family, he embarks on a perilous journey of love and endurance.',
+  };
   constructor(private movies: MoviesService) {}
 
   ngOnInit(): void {
-    this.movies
-      .popularMovies()
-      .subscribe((movies: IPopularMovie[]) => (this.popularMovies = movies));
+    this.movies.popularMovies().subscribe((movies: IPopularMovie[]) => {
+      this.popularMovies = movies;
+      console.log(this.popularMovies);
+    });
   }
 }
