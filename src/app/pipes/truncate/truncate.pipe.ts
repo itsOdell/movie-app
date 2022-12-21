c021e40ba0391c8da926b60fcdecfe8e565dad58
BAbdullaev <abdullaevbilaliddin@gmail.com>
2022-12-21 13:59:30 -0600

    truncate pipe finish

--- src/app/pipes/truncate/truncate.pipe.ts
+++ src/app/pipes/truncate/truncate.pipe.ts
@@ -4,7 +4,13 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate',
 })
 export class TruncatePipe implements PipeTransform {
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, maxLength: number = 20): string {
+    console.log(maxLength);
+    if (value.length > 10) {
+      let slicedString: string = value.slice(0, maxLength);
+      let truncatedString: string = slicedString + '...';
+      return truncatedString;
+    }
+    return value;
   }
 }
